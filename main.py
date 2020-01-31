9f1331620d6af2ec381a6c6daae2e9a5b2f52b74
MasterZer0x <42559276+MasterZer0x@users.noreply.github.com>
2020-01-31 01:22:10 -0300

    Actualizacion sobre Menu Principal
    Interfaz

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame, sys
 from pygame.locals import *
 from Character_class import Personaje
+from colors import *
 
-display_width = 800
-display_height = 600
+display_width = 900
+display_height = 700
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Strategy Tragedy")
 clock = pygame.time.Clock()
@@ -17,19 +18,33 @@ def create_text(text, textFont, color, posX, posY):
     gameDisplay.blit(textSurf,textRect)
 
 
-def buttonMenu(text, w, h, x, y, colorNormal,colorHover,xText, action = None):
+class GameMenu:
+
+    def __init__(self, ):
+
+        self.image_button_SP
+        self.image_button_Coop
+        self.image_button_Prog
+        self.image_button_Exit
+
+def buttonMenu(text, w, h, x, y, imageNormal,imageHover,xText, action = None):
     mouse = pygame.mouse.get_pos()
+
+    imgNormal = pygame.image.load(imageNormal)
+    imgHover = pygame.image.load(imageHover)
+    rect = imgNormal.get_rect()
+    rect.center = (x,y)
     click = pygame.mouse.get_pressed()
-    if x < mouse[0] < x + w and y < mouse[1] < y + h:
-        pygame.draw.rect(gameDisplay, colorHover, (x, y, w, h))
+    if x - w/2 < mouse[0] < x + w/2 and y - h/2 < mouse[1] < y + h/2:
+        gameDisplay.blit( imgHover, rect)
         if click[0] == 1 and action != None:
             action()
 
 
     else:
-        pygame.draw.rect(gameDisplay, colorNormal, (x, y, w, h))
+        gameDisplay.blit( imgNormal, rect)
 
-    create_text(text, smallText, (0,0,0), xText, y+(h/2))
+    create_text(text, smallText, WHITE, xText, y)
 
 
 def Single_Player():
@@ -47,6 +62,7 @@ def Quit():
     pygame.quit()
     quit()
 
+
 def game_menu():
 
     menu = True
@@ -60,31 +76,36 @@ def game_menu():
                 quit()
 
         # Dibujar fondo
-        gameDisplay.fill((255, 255, 255))
+        gameDisplay.fill(BLACK)
 
         # Crear y posicionar Titulo
-        create_text("Strategy Tragedy", Title, (0,0,0), display_width/2, display_height/4)
+        Title_image = pygame.image.load("Graphics/Menu/Title.png")
+        rect = Title_image.get_rect()
+        rect.center = (display_width/2, display_height / 4)
+        gameDisplay.blit(Title_image,rect)
 
         # Parametros Botones
-        buttonWidth = 150
-        buttonHeight = 50
-        PosXbuttonMenu = (display_width - buttonWidth) / 2
+        buttonWidth = 200
+        buttonHeight = 80
+        PosXbuttonMenu = (display_width ) / 2
+        YbuttonMenu = (display_height) / 2 + 10
+        separation = 100
 
         # Button Single Player
-        buttonMenu("Un Jugador",buttonWidth,buttonHeight, PosXbuttonMenu, 250, (0,255,0), (0,100,0), display_width/2, main)
+        buttonMenu("",buttonWidth,buttonHeight, PosXbuttonMenu, YbuttonMenu, "Graphics\Menu\SP_text.png", "Graphics\Menu\SP_Hover_text.png", display_width/2, main)
 
         # Button Coop
-        buttonMenu("Cooperativo", buttonWidth, buttonHeight, PosXbuttonMenu, 250+70*1, (0, 0, 255), (0, 0, 100), display_width / 2, Coop)
+        buttonMenu("Cooperativo", buttonWidth, buttonHeight, PosXbuttonMenu, YbuttonMenu + separation, "Graphics\Menu\Coop.png", "Graphics\Menu\Coop_Hover.png", display_width / 2, Coop)
 
         # Button Programar
-        buttonMenu("Programar", buttonWidth, buttonHeight, PosXbuttonMenu, 250+70*2, (255, 155, 0), (150, 90, 0), display_width / 2, Program)
+        buttonMenu("Programar", buttonWidth, buttonHeight, PosXbuttonMenu, YbuttonMenu + separation*2, "Graphics\Menu\Prog.png", "Graphics\Menu\Prog_Hover.png", display_width / 2, Program)
 
         # Button Exit
-        buttonMenu("Salir", buttonWidth, buttonHeight, PosXbuttonMenu, 250+70*3, (255, 0, 0), (100, 0, 0 ), display_width / 2, Quit)
+        buttonMenu("Salir", buttonWidth, buttonHeight, display_width -60, display_height - 60, "Graphics\Menu\Exit.png", "Graphics\Menu\Exit_Hover.png", display_width -60, Quit)
 
         # Actualizacion pantalla
         pygame.display.update()
-        clock.tick(15)
+        clock.tick(30)
 
 
 
