c3ec0cbf492dcf3e8a7d171b2462610eb421db2c
Avenfenix <thedonjuan008p@gmail.com>
2020-02-05 19:21:29 -0300

    settings y objetos
    ss

--- objects.py
+++ objects.py
@@ -1,26 +1,27 @@
 import pygame, sys
 from pygame.locals import *
 from colors import *
+from settings import *
 vec = pygame.math.Vector2
-velocidad = 2
 
-class Personaje:
+class Personaje(pygame.sprite.Sprite):
     def __init__(self, PosX, PosY):
+        pygame.sprite.Sprite.__init__(self)
         self.Pos = vec(PosX, PosY)
-        self.Vel = vec(2, 0)
+        self.Vel = vec(0, 0)
         self.Acc = vec(0, 0)
         self.WalkCount = 0
         self.draw_quieto = self.get_image("personaje/personaje-principal.png", True)
         self.right = False
         self.left = False
         self.lastDir = "Right"
-        self.hitbox = self.draw_quieto.get_rect()
-        self.hitbox.topleft = self.Pos + vec(25,8)
-        self.hitbox.h = self.hitbox.h - 18
-        self.hitbox.w = self.hitbox.w - 50
+        self.rect = self.draw_quieto.get_rect()
+        self.rect.topleft = self.Pos + vec(25,8)
+        self.rect.h = self.rect.h - 18
+        self.rect.w = self.rect.w - 50
         self.Floor = False
+        self.Col = False
         
-
         # Carga de Sprites
         self.xixf = []
         for i in range(1, 23):
@@ -46,33 +47,33 @@ class Personaje:
         if convertir:
             image = image.convert()
         return image
-
+    
     def teclado(self):
 
-        self.Acc = vec(0, 0.5)
-        self.Acc.x += self.Vel.x * (-0.12)
-        self.Vel.y += self.Acc.y
-        self.Pos.y += self.Vel.y + 0.5 * self.Acc.y
+        self.Acc = vec(0, PLAYER_GRAVITY)
         teclado = pygame.key.get_pressed()
 
         if self.Pos.y > 566:
             self.Pos.y = 566
+            self.Vel.y = 0
             self.Floor = True
         else:
             self.Floor = False
+        
+        
         if teclado[K_RIGHT]:
             if teclado[K_LSHIFT]:
-                self.Pos.x += velocidad*3
+                self.Acc.x += PLAYER_ACC*3
             else:
-                self.Pos.x += velocidad
+                self.Acc.x += PLAYER_ACC
             self.right = True
             self.left = False
             self.lastDir = "Right"
         if teclado[K_LEFT]:
             if teclado[K_LSHIFT]:
-                self.Pos.x -= velocidad*3
+                self.Acc.x -= PLAYER_ACC*3
             else:
-                self.Pos.x -= velocidad
+                self.Acc.x -= PLAYER_ACC
             self.left = True
             self.right = False
             self.lastDir = "Left"
@@ -81,11 +82,16 @@ class Personaje:
             self.left = False
         if self.Floor:
             if teclado[K_SPACE]:
-                self.Vel.y = -10
-            
-        self.hitbox.topleft = self.Pos + vec(23,8)
+                self.Vel.y = -12
+        
+        self.Acc.x += self.Vel.x * (PLAYER_FRICTION)
+        self.Vel.x += self.Acc.x
+        self.Vel.y += self.Acc.y
+        self.Pos += self.Vel + 0.5 * self.Acc
+         
+        self.rect.topleft = self.Pos + vec(23,8)
 
-    def movimiento(self, screen):
+    def dibujar(self, screen):
 
         if self.WalkCount + 1 >= 66:
             self.WalkCount = 0
@@ -102,7 +108,7 @@ class Personaje:
 
         return
 
-class Platform:
+class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((w, h))
@@ -110,44 +116,38 @@ class Platform:
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.rect.w = w
-        self.rect.h = h
     def draw(self,screen):
-        screen.blit(screen, (self.rect.x, self.rect.y))
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
 class colisiones:
     def __init__(self):
-        print("colision")
+        pass
     def collide_player(self, player, target2):
         col_ = False
-        if player.hitbox.colliderect(target2):
+        hits = player.rect.colliderect(target2)
+        if hits:
             col_=True
         return col_
-    def collide_objects(self, target1, target2):
-        col_ = False
-        if target1.rect.colliderect(target2):
-            col_=True
-        return col_
-    def distance_for_player(self, player, target2):
+    def check_distance(self, target1, target2):
         col_= False
-        self.h1 = player.hitbox.h
-        self.w1 = player.hitbox.w
+        self.h1 = target1.rect.h
+        self.w1 = target1.rect.w
         self.h2 = target2.rect.h
         self.w2 = target2.rect.w
-        self.x1 =  player.hitbox.center[0]
-        self.y1 =  player.hitbox.center[1]
+        self.x1 =  target1.rect.center[0]
+        self.y1 =  target1.rect.center[1]
         self.x2 =  target2.rect.center[0]
         self.y2 =  target2.rect.center[1]
         distance = ((self.x1-self.x2)**2 + (self.y1-self.y2)**2)**(1/2)
         max_distance = (((self.h1+self.h2)/2)**2 + ((self.w1+self.w2)/2)**2)**(1/2)
         if (abs(self.x1-self.x2) == (self.w1+self.w2)/2):
-            if distance < max_distance:
+            if distance < max_distance and distance >0:
                 col_ = True
         elif (abs(self.y1-self.y2) == (self.w1+self.w2)/2):
-            if distance < max_distance:
+            if distance < max_distance and distance >0:
                 col_ = True
-        return str(col_)+ " "+str((self.x1, self.y1)) +str((self.x2, self.y2)) + str(distance) +" "+ str(max_distance)
-    
+        return col_
+        #para cualquier objeto
     def distance_for_objects(self, target1, target2):
         col_= False
         self.h1 = target1.rect.h
@@ -167,4 +167,4 @@ class colisiones:
             if distance < max_distance:
                 col_ = True
 
-        return col_
+        return "colisiona: " + str(col_)+ " "+str((self.x1, self.y1)) +str((self.x2, self.y2)) + str(distance) +" "+ str(max_distance)
---
+++ settings.py
@@ -0,0 +1,11 @@
+#Game Options
+
+WIDTH = 900
+HEIGHT = 700
+FPS = 75
+
+#Player Properties
+
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAVITY = 0.5
